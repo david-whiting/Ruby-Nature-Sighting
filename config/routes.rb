fc713f45f6bbf9831b31ae5002f79480d87ec0ce
david-whiting <davidwhiting06@gmail.com>
2020-06-25 16:40:36 -0400

    added sessions_controller, updated routes for login (sessions#new), sessions (sessions#create) and logout(sessions#destroy)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 root :to => "static#home"
 
+get '/login' => "sessions#new"
+post '/sessions' => "sessions#create"
+get '/logout' => "sessions#destroy"
+
 resources :users, :only => [:new, :create] 
 resources :sights
 
 get "/:page" => "static#show"
-
 end
