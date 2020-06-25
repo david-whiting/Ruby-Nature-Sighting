ad9e17ef259070ab025a5f0bb2df4be6cb80fb2a
david-whiting <davidwhiting06@gmail.com>
2020-06-25 16:36:22 -0400

    created view/users/new with a sign up form for new users, tested it in console

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,21 @@
 class UsersController < ApplicationController
 
-	def new
-	end
+    def new
+      @user = User.new
+    end
 
 	def create
+	  	@user = User.new
+	  	@user.name = params[:user][:name]
+	  	@user.email = params[:user][:email]
+		@user.password = params[:user][:password]
+		
+	  if @user.save
+	  	redirect_to root_path #=> GET "/"
+		
+	  else
+	  	render 'users/new' #show them the form
+	  end
 	end
 
 end
